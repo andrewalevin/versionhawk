e01b8c0ee61877ddd9f8033dd42b53ee9c25012b
Andrew Levin <andrewlevingit@hotmail.com>
2024-10-22 20:45:39 +0300

    2024-10-22 20:45:39 N

---
+++ src/dev-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install -e . --no-deps
+echo -e "\033[1;92m\n\nRUN project.script:\n\n\033[0m"
+versionhawk ytb2audiobot
---
+++ src/release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf dist/*
+
+python3 -m pip install --upgrade build
+
+python3 -m pip install --upgrade twine
+
+python3 -m build
+
+python3 -m twine upload dist/*
+
+
